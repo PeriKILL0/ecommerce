80a7ca07d41fc3411fe10906ddceeed3515766f3
PeriKILL0 <100075702+PeriKILL0@users.noreply.github.com>
2022-04-02 20:31:13 +0200

    Prices repository tests

--- prices-service/src/test/java/com/patriarch/ecommerce/prices/repository/PricesRepositoryTest.java
+++ prices-service/src/test/java/com/patriarch/ecommerce/prices/repository/PricesRepositoryTest.java
@@ -1,10 +1,17 @@
 package com.patriarch.ecommerce.prices.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.List;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
@@ -15,12 +22,44 @@ class PricesRepositoryTest {
 	
 	@Autowired
 	PricesRepository repository;
+	
+	@Test
+	void testFindPriceDateFilterFound() throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+		Price expected = new Price();
+		expected.setPriceList(1L);
+		expected.setBrandId(1L);
+		expected.setProductId(1L);
+		expected.setStartDate(sdf.parse("2022-04-01 00:00"));
+		expected.setEndDate(sdf.parse("2022-05-01 00:00"));
+		expected.setPriority(0);
+		expected.setValue(35.99);
+		expected.setCurrency("EUR");
+		repository.save(expected);
+		
+		List<Price> prices = repository.findPrice(1L, 1L, sdf.parse("2022-04-04 00:00"));
+		
+		assertThat(prices).isNotEmpty();
+		assertThat(prices.get(0)).usingRecursiveComparison().isEqualTo(expected);
+	}
 
 	@Test
-	void test() {
+	void testFindPriceDateFilterNotFound() throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+		Price expected = new Price();
+		expected.setPriceList(1L);
+		expected.setBrandId(1L);
+		expected.setProductId(1L);
+		expected.setStartDate(sdf.parse("2022-04-01 00:00"));
+		expected.setEndDate(sdf.parse("2022-05-01 00:00"));
+		expected.setPriority(0);
+		expected.setValue(35.99);
+		expected.setCurrency("EUR");
+		repository.save(expected);
 		
-		Price price = repository.findPriceByProductoAndBrandAndDate(1L, 1L, new Date());
+		List<Price> prices = repository.findPrice(1L, 1L, sdf.parse("2022-05-04 00:00"));
 		
+		assertThat(prices).isEmpty();
 	}
 
 }
