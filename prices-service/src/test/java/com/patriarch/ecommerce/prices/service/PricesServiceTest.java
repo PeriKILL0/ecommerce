ff834a4f4488e0b841789a8fd83aaf7e1268a2d4
PeriKILL0 <100075702+PeriKILL0@users.noreply.github.com>
2022-04-02 21:18:34 +0200

    Prices service test
    Corrected date format.

--- prices-service/src/test/java/com/patriarch/ecommerce/prices/service/PricesServiceTest.java
+++ prices-service/src/test/java/com/patriarch/ecommerce/prices/service/PricesServiceTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -17,71 +19,57 @@ import com.patriarch.ecommerce.prices.entity.Price;
 @SpringBootTest
 class PricesServiceTest {
 
-	private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-
 	@Autowired
 	PricesService pricesService;
-	
-	@ParameterizedTest
-	@ValueSource(strings = {"2020-06-14 10:00","2020-06-14 18:00","2020-06-14 16:00","2020-06-15 10:00","2020-06-16 21:00"}, 
-	longs = {1,2,1,3,4})
-	void testFindPrice(String date, long priceList) throws ParseException {
-		
-		Price price = pricesService.findPrice(35455L, 1L, sdf.parse(date));
-		
-		assertThat(price).isNotNull();
-		assertThat(price.getPriceList()).isEqualTo(date);
-		
-	}
-	
+
 	@Test
 	void testFindPriceCase1() throws ParseException {
-		
-		Price price = pricesService.findPrice(35455L, 1L, sdf.parse("2020-06-14 10:00"));
-		
+
+		Price price = pricesService.findPrice(35455L, 1L, Date.from(Instant.parse("2020-06-14T10:00:00Z")));
+
 		assertThat(price).isNotNull();
 		assertThat(price.getPriceList()).isEqualTo(1L);
-		
+
 	}
-	
+
 	@Test
 	void testFindPriceCase2() throws ParseException {
-		
-		Price price = pricesService.findPrice(35455L, 1L, sdf.parse("2020-06-14 18:00"));
-		
+
+		Price price = pricesService.findPrice(35455L, 1L, Date.from(Instant.parse("2020-06-14T16:00:00Z")));
+
 		assertThat(price).isNotNull();
 		assertThat(price.getPriceList()).isEqualTo(2L);
-		
+
 	}
-	
+
 	@Test
 	void testFindPriceCase3() throws ParseException {
-		
-		Price price = pricesService.findPrice(35455L, 1L, sdf.parse("2020-06-14 16:00"));
-		
+
+		Price price = pricesService.findPrice(35455L, 1L, Date.from(Instant.parse("2020-06-14T21:00:00Z")));
+
 		assertThat(price).isNotNull();
 		assertThat(price.getPriceList()).isEqualTo(1L);
-		
+
 	}
-	
+
 	@Test
 	void testFindPriceCase4() throws ParseException {
-		
-		Price price = pricesService.findPrice(35455L, 1L, sdf.parse("2020-06-15 10:00"));
-		
+
+		Price price = pricesService.findPrice(35455L, 1L, Date.from(Instant.parse("2020-06-15T10:00:00Z")));
+
 		assertThat(price).isNotNull();
 		assertThat(price.getPriceList()).isEqualTo(3L);
-		
+
 	}
-	
+
 	@Test
 	void testFindPriceCase5() throws ParseException {
-		
-		Price price = pricesService.findPrice(35455L, 1L, sdf.parse("2020-06-16 21:00"));
-		
+
+		Price price = pricesService.findPrice(35455L, 1L, Date.from(Instant.parse("2020-06-16T21:00:00Z")));
+
 		assertThat(price).isNotNull();
 		assertThat(price.getPriceList()).isEqualTo(4L);
-		
+
 	}
 
 }
